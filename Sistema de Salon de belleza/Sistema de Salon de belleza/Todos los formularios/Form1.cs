1f0a0e42fa54e9ceca6c55bc3f554226f4f85cca
Allan Lopez <allanlopezxd28@gmail.com>
2021-06-16 21:28:44 -0600

    Agregando Proyecto primer avance

---
+++ Sistema de Salon de belleza/Sistema de Salon de belleza/Todos los formularios/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Salon_de_belleza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBox1.Checked == true)
+            TextBox2.UseSystemPasswordChar = false;
+            else
+                TextBox2.UseSystemPasswordChar = true;
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string usuario1,usuario2;
+            string contraseña1, contraseña2;
+
+            usuario1 = "Henry"; usuario2 = "Gladis";
+            contraseña1 = "Henry19"; contraseña2 = "Gladis15";
+            
+            if ((TextBox1.Text == usuario1 && TextBox2.Text == contraseña1) || (TextBox1.Text == usuario2 && TextBox2.Text == contraseña2))
+            {
+                MessageBox.Show ("--- BIENVENIDO ---");
+               
+                this.Close();
+                
+            }             
+        else
+            {
+                MessageBox.Show("--Contraseña o usuario incorrecto--");
+            }
+
+        }
+    }
+}
